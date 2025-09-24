5271e20ebca1b4add9cb2866e8b03840f0310755
myselfleo <myselfleo@outlook.fr>
2025-09-24 23:14:23 +0200

    tried to optimise pass 2

--- src/wrld/shaders/deferred_pass2.glsl
+++ src/wrld/shaders/deferred_pass2.glsl
@@ -53,16 +53,15 @@ uniform PointLight point_lights[MAX_LIGHTS];
 uniform uint directional_lights_nb;
 uniform DirectionalLight directional_lights[MAX_LIGHTS];
 
+struct FragData {
+    vec3 pos;
+    vec3 normal;
+    vec3 diffuse;
+    float specular;
+};
 
 out vec4 FragColor;
 
-vec3 sample_diffuse(vec2 uv) {
-    return texture(diffuse_texture, uv).rgb;
-}
-
-float sample_specular(vec2 uv) {
-    return max(1 - texture(diffuse_texture, uv).a, 1.0);
-}
 
 float calc_diffuse(vec3 light_direction, vec3 normal) {
     return max(dot(normal, light_direction), 0.0);
@@ -73,78 +72,80 @@ float calc_specular(vec3 light_direction, vec3 view_direction, vec3 normal) {
     return pow(max(dot(normal, halfway_direction), 0.0), 32);
 }
 
-vec4 calc_ambiant_light(vec2 uv) {
-    return vec4(ambiant_light.color * sample_diffuse(uv), 1.0) * ambiant_light.intensity;
+vec4 calc_ambiant_light(FragData data) {
+    return vec4(ambiant_light.color * data.diffuse, 1.0) * ambiant_light.intensity;
 }
 
 /// Compute the color contribution of the given PointLight for the fragment
-vec4 calc_point_light(PointLight pl, vec2 uv) {
-    vec3 frag_pos = texture(position_texture, uv).rgb;
-    vec3 normal = normalize(texture(normal_texture, uv).rgb);
-
+vec4 calc_point_light(PointLight pl, FragData data, vec3 view_direction) {
     // Attenuation due to the distance from the point light
     // todo: Maybe use the sphere area equation
-    float distance = length(pl.position - frag_pos);
+    float distance = length(pl.position - data.pos);
 
     float attenuation = 0.0;
     if (pl.intensity > 0) {
         attenuation = pl.intensity / (pl.intensity + distance * distance);
     }
 
-    vec3 light_direction = normalize(pl.position - frag_pos);
-    vec3 view_direction = normalize(view_pos - frag_pos);
+    vec3 light_direction = normalize(pl.position - data.pos);
 
-    float diffuse_amount = calc_diffuse(light_direction, normal);
-    float specular_amount = calc_specular(light_direction, view_direction, normal);
+    float diffuse_amount = calc_diffuse(light_direction, data.normal);
+    float specular_amount = calc_specular(light_direction, view_direction, data.normal);
 
-    vec4 diffuse = vec4(pl.color * sample_diffuse(uv), 1.0);
-    vec3 specular = pl.color * sample_specular(uv);
-
-    //return vec4(distance / 10.0, distance / 10.0, distance / 10.0, 1.0);
+    vec4 diffuse = vec4(pl.color * data.diffuse, 1.0);
+    vec3 specular = pl.color * data.specular;
 
     // todo : specular is probably not correct for transparent values
-    return (diffuse * diffuse_amount + vec4(specular * specular_amount, sample_specular(uv))) * pl.intensity * attenuation;
+    return (diffuse * diffuse_amount + vec4(specular * specular_amount, data.specular)) * pl.intensity * attenuation;
 }
 
 /// Compute the color contribution of the given DirectionalLight for the fragment
-vec4 calc_directional_light(DirectionalLight dl, vec2 uv) {
-    vec3 frag_pos = texture(position_texture, uv).rgb;
-    vec3 normal = normalize(texture(normal_texture, uv).rgb);
-
+vec4 calc_directional_light(DirectionalLight dl, FragData data, vec3 view_direction) {
     vec3 light_direction = normalize(-dl.direction);
-    vec3 view_direction = normalize(view_pos - frag_pos);
 
-    float diffuse_amount = calc_diffuse(light_direction, normal);
-    float specular_amount = calc_specular(light_direction, view_direction, normal);
+    float diffuse_amount = calc_diffuse(light_direction, data.normal);
+    float specular_amount = calc_specular(light_direction, view_direction, data.normal);
 
-    vec4 diffuse = vec4(dl.color * sample_diffuse(uv), 1.0);
-    vec3 specular = dl.color * sample_specular(uv);
+    vec4 diffuse = vec4(dl.color * data.diffuse, 1.0);
+    vec3 specular = dl.color * data.specular;
 
-    return (diffuse * diffuse_amount + vec4(specular * specular_amount, sample_specular(uv))) * dl.intensity;
+    return (diffuse * diffuse_amount + vec4(specular * specular_amount, data.specular)) * dl.intensity;
+}
+
+FragData sample_data() {
+    vec2 uv = gl_FragCoord.xy / vec2(textureSize(diffuse_texture, 0));
+
+    FragData res;
+    res.pos = texture(position_texture, uv).rgb;
+    res.normal = normalize(texture(normal_texture, uv).rgb);
+    res.diffuse = texture(diffuse_texture, uv).rgb;
+    res.specular = max(1 - texture(diffuse_texture, uv).a, 1.0);
+
+    return res;
 }
 
 void main()
 {
-    vec2 uv = gl_FragCoord.xy / vec2(textureSize(diffuse_texture, 0));
+    FragData data = sample_data();
 
     vec4 res = vec4(0.0);
 
     // Process ambient light
-    res += calc_ambiant_light(uv);
+    res += calc_ambiant_light(data);
+
+    vec3 view_direction = normalize(view_pos - data.pos);
 
     // Process point lights
     for (int i = 0; i < point_light_nb; i++) {
-        res += calc_point_light(point_lights[i], uv);
+        res += calc_point_light(point_lights[i], data, view_direction);
     }
 
     // Process directional lights
     for (int i = 0; i < directional_lights_nb; i++) {
-        res += calc_directional_light(directional_lights[i], uv);
+        res += calc_directional_light(directional_lights[i], data, view_direction);
     }
 
     FragColor = res;
-
-    //FragColor = calc_point_light(point_lights[0], uv);
 }
 
 #endif
