9104046de0908e80559c52741d97a6f92c88b2be
myselfleo <myselfleo@outlook.fr>
2025-12-12 13:32:14 +0100

    put Singleton in wrld namespace

--- include/wrld/Singleton.hpp
+++ include/wrld/Singleton.hpp
@@ -4,21 +4,23 @@
 
 #pragma once
 
-/// This
-template<typename T>
-class Singleton {
-protected:
-    Singleton() = default;
+namespace wrld {
+    template<typename T>
+    class Singleton {
+    protected:
+        Singleton() = default;
 
-    virtual ~Singleton() = default;
+        virtual ~Singleton() = default;
 
-public:
-    Singleton(const Singleton &) = delete;
+    public:
+        Singleton(const Singleton &) = delete;
 
-    Singleton &operator=(const Singleton &) = delete;
+        Singleton &operator=(const Singleton &) = delete;
 
-    static T *get() {
-        static T instance;
-        return &instance;
-    }
-};
+        static T *get() {
+            static T instance;
+            return &instance;
+        }
+    };
+
+}
